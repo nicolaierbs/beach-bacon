d91dde102aaeecc5cd026250b703a850b743c186
Nicolai Erbs <nicolai.erbs@infomotion.de>
2020-12-26 16:16:56 +0100

    methods

--- graph_tool.py
+++ graph_tool.py
@@ -6,32 +6,41 @@ import numpy as np
 import pandas as pd
 
 
+def compute_connectors(graph):
+    nodes = list(graph.get_data().keys())
+
+    column_names = ['player', 'longest_path', 'mean_path', 'count_nan', 'player_count']
+    df = pd.DataFrame(columns=column_names)
+    for node_one in nodes:
+        paths = np.empty([0], dtype=int)
+        for node_two in nodes:
+            try:
+                paths = np.append(paths, find_path(graph, node_one, node_two).total_cost)
+            except NoPathError as e:
+                paths = np.append(paths, np.nan)
+
+        df = df.append({'player': node_one,
+                        'longest_path': np.nanmax(paths),
+                        'mean_path': np.nanmean(paths),
+                        'count_nan': np.count_nonzero(np.isnan(paths)),
+                        'player_count': len(paths)}, ignore_index=True)
+
+    return df
+
+
 def build_graph():
-    team_graph = Graph(undirected=True)
+    graph = Graph(undirected=True)
     for team in mongo_connector.retrieve_teams():
         if len(team['player']) == 2:
-            team_graph.add_edge(team['player'][0], team['player'][1], 1)
-    return team_graph
+            graph.add_edge(team['player'][0], team['player'][1], 1)
+    return graph
 
 
-graph = build_graph()
-nodes = list(graph.get_data().keys())
+team_graph = build_graph()
+team_graph.dump('full.graph')
+print('exported graph')
+team_graph = Graph.load('full.graph')
 
-column_names = ['player', 'longest_path', 'mean_path', 'count_nan', 'player_count']
-df = pd.DataFrame(columns=column_names)
-for node_one in nodes:
-    paths = np.empty([0], dtype=int)
-    for node_two in nodes:
-        try:
-            paths = np.append(paths, find_path(graph, node_one, node_two).total_cost)
-        except NoPathError as e:
-            paths = np.append(paths, np.nan)
-
-    df = df.append({'player': node_one,
-                    'longest_path': np.nanmax(paths),
-                    'mean_path': np.nanmean(paths),
-                    'count_nan': np.count_nonzero(np.isnan(paths)),
-                    'player_count': len(paths)}, ignore_index=True)
-
-    df.to_pickle('shortest_paths.pkl')
-    print(df.describe())
+paths = compute_connectors(team_graph)
+paths.to_pickle('shortest_paths.pkl')
+print(paths.describe())
